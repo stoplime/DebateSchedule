4a96f3311446ef377d4dcd61841643428edf587f
stoplime <slanimero@gmail.com>
2015-11-30 03:25:32 -0500

    added rank

---
+++ BussinessObject/Rank.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataAccess;
+
+namespace BussinessObject
+{
+    public class Rank
+    {
+        private RankData rankData;
+
+        private List<string> teamNames;
+        public List<string> TeamNames
+        {
+            get { return teamNames; }
+        }
+
+        private List<int> teamScores;
+        public List<int> TeamScores
+        {
+            get { return teamScores; }
+        }
+
+        private List<int> teamWins;
+        public List<int> TeamWins
+        {
+            get { return teamWins; }
+        }
+
+        private List<int> teamTotalMatches;
+        public List<int> TeamTotalMatches
+        {
+            get { return teamTotalMatches; }
+        }
+
+        public Rank()
+        {
+            rankData = new RankData();
+            rankData.GetRanks(out teamNames, out teamScores, out teamWins, out teamTotalMatches);
+        }
+    }
+}
---
+++ DataAccess/RankData.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace DataAccess
+{
+    public class RankData
+    {
+        private DatabaseLayer dl;
+
+        public RankData()
+        {
+            dl = new DatabaseLayer();
+        }
+
+        public bool GetRanks(out List<string> teamNames, out List<int> teamScores, out List<int> teamWins, out List<int> teamTotalMatches)
+        {
+            dl.SqlConnection.Open();
+            string getRanksString = "SELECT * FROM Team WHERE team_deleted IS NULL ORDER BY team_wins DESC, team_score DESC, team_name";
+            SqlCommand sqlc = new SqlCommand(getRanksString, dl.SqlConnection);
+            SqlDataReader reader = sqlc.ExecuteReader();
+            teamNames = new List<string>();
+            teamScores = new List<int>();
+            teamWins = new List<int>();
+            teamTotalMatches = new List<int>();
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    teamNames.Add(reader.GetString(1));
+                    if (reader.GetValue(3) != DBNull.Value)
+                    {
+                        teamScores.Add(reader.GetInt32(3));
+                    }
+                    else
+                    {
+                        teamScores.Add(0);
+                    }
+                    if (reader.GetValue(4) != DBNull.Value)
+                    {
+                        teamWins.Add(reader.GetInt32(4));
+                    }
+                    else
+                    {
+                        teamWins.Add(0);
+                    }
+                    if (reader.GetValue(5) != DBNull.Value)
+                    {
+                        if (reader.GetInt32(5) != 0)
+                        {
+                            teamTotalMatches.Add(reader.GetInt32(5));
+                        }
+                        else
+                        {
+                            teamTotalMatches.Add(-1);
+                        }
+                    }
+                    else
+                    {
+                        teamTotalMatches.Add(-1);
+                    }
+                }
+            }
+            dl.SqlConnection.Close();
+            return true;
+        }
+
+    }
+}
