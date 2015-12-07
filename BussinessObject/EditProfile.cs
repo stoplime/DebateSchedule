b9e1e01e087f32d2179bf139027cf550c4198022
stoplime <slanimero@gmail.com>
2015-12-07 02:41:39 -0500

    qick fix for Jaylin

---
+++ BussinessObject/EditProfile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DataAccess;
+
+namespace BussinessObject
+{
+    public class EditProfile
+    {
+        private EditProfileData accessData;
+
+        private string username;
+        public string Username
+        {
+            get { return username; }
+        }
+
+        private string email;
+        public string Email
+        {
+            get { return email; }
+        }
+
+        private string firstName;
+        public string FirstName
+        {
+            get { return firstName; }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get { return LastName; }
+        }
+
+        public EditProfile()
+        {
+            accessData = new EditProfileData();
+        }
+
+        public bool GetData(int userID)
+        {
+            accessData.GetData(userID, out username, out email, out firstName, out lastName);
+            return true;
+        }
+    }
+}
---
+++ DataAccess/EditProfileData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace DataAccess
+{
+    public class EditProfileData
+    {
+        private DatabaseLayer dl;
+        
+        public EditProfileData()
+        {
+            dl = new DatabaseLayer();
+        }
+
+        public bool GetData(int userID, out string username, out string email, out string firstname, out string lastname)
+        {
+            dl.SqlConnection.Open();
+            string getDataString = "SELECT users_login, users_email FROM Users WHERE users_id='" + userID + "'";
+            SqlCommand sqlc = new SqlCommand(getDataString, dl.SqlConnection);
+            SqlDataReader reader = sqlc.ExecuteReader();
+            username = "";
+            email = "";
+            firstname = "";
+            lastname = "";
+            if (reader.HasRows)
+            {
+                if (reader.Read())
+                {
+                    username = reader.GetString(0);
+                    email = reader.GetString(1);
+                }
+            }
+            reader.Close();
+            //string getDataString = "SELECT users_login, users_email FROM Users WHERE users_id='" + userID + "'";
+            //SqlCommand sqlc = new SqlCommand(getDataString, dl.SqlConnection);
+            //SqlDataReader reader = sqlc.ExecuteReader();
+            dl.SqlConnection.Close();
+            return true;
+        }
+
+    }
+}
